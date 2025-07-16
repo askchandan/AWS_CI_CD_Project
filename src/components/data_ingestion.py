3d7085abc44fc384bd3adec682a82098e610b3f8
Chandan Malakar <127213898+askchandan@users.noreply.github.com>
2025-07-16 13:34:24 +0530

    Update data_ingestion.py

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -17,9 +17,9 @@ os.environ['LOKY_MAX_CPU_COUNT'] = '16'  # To avoid issues with multiprocessing 
 
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str = os.path.join('artifacts', 'train.csv')
-    test_data_path: str = os.path.join('artifacts', 'test.csv')
-    raw_data_path: str = os.path.join('artifacts', 'raw.csv')
+    train_data_path: str = os.path.join(os.getcwd(),'artifacts', 'train.csv')
+    test_data_path: str = os.path.join(os.getcwd(),'artifacts', 'test.csv')
+    raw_data_path: str = os.path.join(os.getcwd(),'artifacts', 'raw.csv')
 
 class DataIngestion:
     def __init__(self):
